9efdf5f9b68a96d46fa729caea11bd00f298671a
AdityaSahu786 <sahuadi786183@gmail.com>
2024-12-12 12:34:03 +0530

    adminMiddleware

---
+++ Week_4.3/admin.js
@@ -0,0 +1,8 @@
+//Middleware for handling auth
+function adminMiddleware(req, res, next){
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin
+    // from the admin DB
+    // check readme for the exact headers to be expected
+}
+module.exports = adminMiddleware;
