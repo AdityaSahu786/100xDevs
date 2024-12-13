78512ec39b7844029a7ca19db9825e220d85bb95
AdityaSahu786 <sahuadi786183@gmail.com>
2024-12-14 04:14:27 +0530

    Todo map function

--- Week_5.1/counter-app/src/App.jsx
+++ Week_5.1/counter-app/src/App.jsx
@@ -9,12 +9,21 @@ function App() {
         title: "Study DSA",
         description: "Study DSA from 9-10 pm",
         completed: true
+    }, {
+        title: "Maintain a good cgpa",
+        description: "try to maintain a 8.5+ cgpa",
+        completed: false
     }]);
 
    return (
-    <div>
+    /* <div>
         <Todo title={todos[0].title} description={todos[0].description}></Todo>
         <Todo title={todos[1].title} description={todos[1].description}></Todo>
+    </div> */
+    <div>
+        {todos.map(function(todo) {
+            return <Todo title={todo.title} description={todo.description} />
+        })}
     </div>
    )
 }
