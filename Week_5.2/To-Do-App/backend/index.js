14c3eef61a852b790ad71a5f07d1ea976db7bc11
AdityaSahu786 <sahuadi786183@gmail.com>
2024-12-14 13:11:53 +0530

    Validations

--- Week_5.2/To-Do-App/backend/index.js
+++ Week_5.2/To-Do-App/backend/index.js
@@ -2,6 +2,7 @@
 // with express.json() middleware
 
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 const port = 3000;
 
@@ -13,7 +14,14 @@ app.use(express.json());
 } */
 
 app.post("/todo", function(req, res) {
-
+      const createPayload = req.body;
+      const parsePayload =  createTodo.safeParse(createPayload);
+      if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+      }
 })
 
 app.get("/todos", function (req, res)   {
@@ -21,7 +29,14 @@ app.get("/todos", function (req, res)   {
 });
 
 app.put("/completed", function (req, res)  {
-
+     const updatePayload = req.body;
+     const parsePayload = updateTodo.safeParse(updatePayload);
+     if(!updatePayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+     }
 });
 
 app.listen(port);
